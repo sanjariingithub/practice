5a39e2696296c2436c3e3b75f9b4168bd797523f
sanjariingithub <sanjarir018@gmail.com>
2022-08-23 20:24:56 +0600

    first commit

--- script.js
+++ script.js
@@ -7,6 +7,40 @@ jQuery(function ($) {
         meanScreenWidth: "1099"
     });
 
+    $('.owl-two').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:0
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+    $('.owl-three').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:0
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
     $('.owl-carousel').owlCarousel({
         loop:false,
         margin:0,
@@ -18,7 +52,14 @@ jQuery(function ($) {
             }
            
         }
+
+      
     });
+    
+    
+
+
+   
     
 
 });
